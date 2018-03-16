fc554c9265c693fac8706f923097e5eff43f28c1
Carlos Li Hu <carloslihu96@gmail.com>
2018-03-16 17:43:47 +0100

    ejercicio 3 hecho

---
+++ P2/limpiar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
+	echo "###########################################################"
+	echo $i
+	echo "###########################################################"
+	cd $i
+	ant replegar; ant delete-pool-local limpiar-todo
+	cd -
+done
+cd P1-base
+ant delete-db
+cd -
