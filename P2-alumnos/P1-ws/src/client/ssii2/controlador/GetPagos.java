9d12be3a59eaa26d31e767252103679c075f61b1
Carlos Li Hu <carloslihu96@gmail.com>
2018-03-16 12:46:00 +0100

    cambiados build.properties

---
+++ P2-alumnos/P1-ws/src/client/ssii2/controlador/GetPagos.java
@@ -0,0 +1,111 @@
+/**
+ * Pr&aacute;ctricas de Sistemas Inform&aacute;ticos II
+ * 
+ * Esta servlet se encarga de visualizar los pagos para un determinado comercio. 
+ * Es necesario que en la llamada se incluya un valor correcto del par&aacute;metros:
+ * <dl>
+ *    <dt>Identificador del comercio</dt>
+ *    <dd>Este identificador es &uacute;nico para cada comercio. Se provee al comercio
+ * tras la firma del contrato de utilizaci&oacute;n del sistema de pago. </dd>
+ * </dl>
+ */
+
+package ssii2.controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ssii2.visa.PagoBean;
+//import ssii2.visa.dao.VisaDAO;
+import ssii2.visa.VisaDAOWSService; // Stub generado automáticamente
+import ssii2.visa.VisaDAOWS; // Stub generado automáticamente
+import javax.xml.ws.WebServiceRef;
+import javax.xml.ws.BindingProvider;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author phaya
+ */
+public class GetPagos extends ServletRaiz {
+     
+    /** 
+     * Par&aacute;metro que indica el identificador de comercio
+     */
+    public final static String PARAM_ID_COMERCIO = "idComercio";
+
+    /** 
+     * Par&aacute;metro que indica la ruta de retorno
+     */
+    public final static String PARAM_RUTA_RETORNO = "ruta";
+
+    /** 
+     * Atribute que hace referencia a la lista de pagos
+     */
+    public final static String ATTR_PAGOS = "pagos";
+    
+    /** 
+    * Procesa una petici&oacute;n HTTP tanto <code>GET</code> como <code>POST</code>.
+    * @param request objeto de petici&oacute;n
+    * @param response objeto de respuesta
+    */    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {        
+        
+		VisaDAOWSService service = new VisaDAOWSService();
+		VisaDAOWS dao = service.getVisaDAOWSPort();
+		String url = getServletContext().getInitParameter("url-server");
+		try {
+			BindingProvider bp = (BindingProvider) dao;
+			bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+		} catch (Exception e){
+			enviaError(new Exception("servidor no encontrado"), request, response);
+			return;
+		}
+		
+		/* Se recoge de la petici&oacute;n el par&aacute;metro idComercio*/  
+		String idComercio = request.getParameter(PARAM_ID_COMERCIO);
+		
+		/* Petici&oacute;n de los pagos para el comercio */
+		List <PagoBean> pagosList = dao.getPagos(idComercio);
+		PagoBean[] pagos = pagosList.toArray(new PagoBean[pagosList.size()]);
+
+        request.setAttribute(ATTR_PAGOS, pagos);
+        reenvia("/listapagos.jsp", request, response);
+        return;       
+    }      
+    
+   /** 
+    * Procesa una petici&oacute;n HTTP <code>GET</code>.
+    * @param request objeto de petici&oacute;n
+    * @param response objeto de respuesta
+    */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+    * Procesa una petici&oacute;n HTTP <code>POST</code>.
+    * @param request objeto de petici&oacute;n
+    * @param response objeto de respuesta
+    */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** 
+    * Devuelve una descripici&oacute;n abreviada del servlet
+    */
+    @Override
+    public String getServletInfo() {
+        return "Servlet Get Pagos";
+    }
+
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ SI2P1A_2401_2.zip
 
 
 Igual hay que modificar las ips de los servidores para P2 que el jmeter sepa a donde enviar las cosas bien (?)
+
+Máquina virtual VMWare con: 1 CPU y 768 MB de RAM
